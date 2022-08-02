f47dfe2ef67553f1b9c88d1e2ad5f367c8f90e98
Andrey Smolyar <smolyar2022@internet.ru>
2022-08-02 22:14:41 +0500

    Задание №2

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-let title;
-let screens;
-const screenPrice;
-const rollback;
-let fullPrice;
-const adaptive;
+const title = "Project";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 1000;
+let rollback = 90;
+let fullPrice = 1000000;
+let adaptive = true;
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+
+console.log("Стоимость верстки экранов ", screenPrice, " рублей/долларов/гривен/юани");
+console.log("Стоимость разработки сайта ", fullPrice, " рублей/долларов/гривен/юани");
+
+console.log(screens.toLowerCase());
+console.log(screens.split());
+
+console.log("Процент отката посреднику за работу", (fullPrice * (rollback / 100)));
