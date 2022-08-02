c904b03d25d889434118e7ce4c2b593e3636bacd
Andrey Smolyar <smolyar2022@internet.ru>
2022-08-02 21:19:23 +0500

    Добавтл console.log и alert

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-let title;
-let screens;
-const screenPrice;
-const rollback;
-let fullPrice;
-const adaptive;
+let title = 1;
+let screens = 1;
+const screenPrice = 2;
+const rollback = 3;
+let fullPrice = 14;
+const adaptive = 3;
+
+alert("Hello world!");
+console.log("Hello world!");
