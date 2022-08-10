b8fd77e5582c24907e400e7ddc34199905cd3839
Andrey Smolyar <smolyar2022@internet.ru>
2022-08-10 19:29:54 +0500

    задание05

--- script.js
+++ script.js
@@ -3,21 +3,23 @@
 let title = prompt('Как называется ваш проект?');
 let screens = prompt('Какие типы экранов нужно разработать?');
 let screenPrice = prompt('Сколько будет стоить данная работа?');
-let service1 = prompt('Какой дополнительный тип услуги нужен?');
-let servicePrice1 = prompt('Сколько это будет стоить?');
-let service2 = prompt('Какой дополнительный тип услуги нужен?');
-let servicePrice2 = prompt('Сколько это будет стоить?');
+
 let rollback = 10;
 let fullPrice = getFullPrice();
 let adaptive = confirm('Нужен ли адаптив на сайте?');
 let servicePercentPrice = getServicePercentPrices();
+
+let service1 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice1 = prompt('Сколько это будет стоить?');
+let service2 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice2 = prompt('Сколько это будет стоить?');
 
 //lesson 04-----------------------------------
 let allServicePrices = getAllServicePrices();
 //lesson 04-----------------------------------
 
 //lesson 04-----------------------------------
-function getAllServicePrices(servicePrice1, servicePrice2) {
+function getAllServicePrices() {
     return servicePrice1 + servicePrice2;
 }
 
