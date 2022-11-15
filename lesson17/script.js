515b13eb4606b9d0e3a9e064b19daf35145a5d1f
Andrey Smolyar <smolyar2022@internet.ru>
2022-11-15 21:03:57 +0500

    lesson17

--- lesson17/script.js
+++ lesson17/script.js
@@ -7,32 +7,60 @@ class Worker {
         this.lastName = lastName;
         this.age = age;
         this.position = position;
+        Worker.incrementCount();
+    }
+
+    static count = 0;
+
+    static getCount() {
+        return Worker.count;
+    }
+
+    static incrementCount() {
+        Worker.count++;
     }
 };
 
 const worker = new Worker();
 
+console.log(Worker.getCount());
+
+
 class Locksmith extends Worker {
 
-    constructor(name, lastName, age, position, organisation, category, skills = []) {
+    constructor(name, lastName, age, position, organisation, category, skills = [], married) {
         super(name, lastName, age, position);
         this._organisation = organisation;
         this.category = category;
         this._skills = skills;
+        this.married = married;
     }
 
-    get organisation() {
+    sayHello() {
+        console.log(`Привет! меня зовут ${this.name}. Мне ${this.age} лет`);
+    }
+
+    get skills() {
         return this._skills;
     };
 
     set skills(str) {
-        this.skills.push(str)
+        this._skills.push(str)
     }
 };
 
 const locksmith = new Locksmith();
 
+locksmith.name = 'Alex';
+locksmith.age = 33;
+locksmith.position = 'master';
+locksmith.organisation = 'OOO MRK';
+locksmith.category = '6'; ''
 locksmith.skills = 'welding';
+locksmith.skills = 'electricTools';
+locksmith.married = true;
+
+locksmith.sayHello();
 
 
 class Driver extends Worker {
@@ -43,10 +71,41 @@ class Driver extends Worker {
         this.dateOfEmployment = dateOfEmployment;
     }
 
+    sayHello() {
+        console.log(`Привет! Меня зовут ${this.name}. Моя работа - ${this.position}`);
+    }
+
     get children() {
         return this._children;
+    }
+
+    set children(str) {
+        this._children.push(str)
     }
 };
 
 const driver = new Driver();
 
+driver.name = 'Igor';
+driver.age = 39;
+driver.position = 'gazelDriver';
+driver.organisation = 'OOO MMK';
+driver.category = '4';
+
+driver.sayHello();
+
+console.log(locksmith.name);
+console.log(locksmith.age);
+console.log(locksmith.skills);
+console.log(locksmith.organisation);
+
+console.log(driver.name);
+console.log(driver.age);
+console.log(driver.position);
+console.log(driver.organisation);
+console.log(driver.category);
+
+// chicking the weight of our objects 
+
+console.log(JSON.stringify(locksmith).length);
+console.log(JSON.stringify(driver).length);
