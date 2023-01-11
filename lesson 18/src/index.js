36b1d9221c23e0c130a762bc730842430ff2bca1
Andrey Smolyar <smolyar2022@internet.ru>
2023-01-11 19:54:53 +0500

    lesson18

---
+++ lesson 18/src/index.js
@@ -0,0 +1,1 @@
+const burger = require('./modules/burger')
---
+++ lesson 18/src/modules/slider.js
@@ -0,0 +1,7 @@
+const sliderModule = () => {
+    const slider = 'my slider'
+
+    console.log(slider);
+}
+
+module.exports = sliderModule
